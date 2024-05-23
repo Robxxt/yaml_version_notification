7319525c6d89bcb5750ecac22f0f5fff04c2124c
Robxxt <73996637+Robxxt@users.noreply.github.com>
2024-05-23 12:40:24 +0000

    create config file if doesn't exists

--- lib/pre_commit_handler.py
+++ lib/pre_commit_handler.py
@@ -27,7 +27,8 @@ create .pre-commit-config.yaml template file
 '''
 def create_pre_commit_config_file():
     print("[INFO]: .pre-commit-config.yaml doesn't exists. Creating one based on a template. Feel free to modify it!")
-
+    with open('.pre-commit-config.yaml', 'w') as file:
+        file.write('---\nThis is a template pre-commit-config file')    
 
 '''
 run auto-update
