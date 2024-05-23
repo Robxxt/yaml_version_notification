a6972e941b478bad1c630f891991ba256ff21790
Robxxt <73996637+Robxxt@users.noreply.github.com>
2024-05-23 13:40:27 +0000

    auto update basic implemntation

--- lib/pre_commit_handler.py
+++ lib/pre_commit_handler.py
@@ -33,3 +33,13 @@ def create_pre_commit_config_file():
 '''
 run auto-update
 '''
+def auto_update_pre_commit_config():
+    try:
+        command = subprocess.run('pre-commit auto-update', shell=True)
+        # still to see what is the valid output code
+        if (command.returncode != 0):
+            print('Invalid [pre-commit auto-update]', file=sys.stderr)
+            sys.exit(1)
+    except:
+        print('There was a problem executing [pre-commit auto-update]', file=sys.stderr)
+        sys.exit(1)
