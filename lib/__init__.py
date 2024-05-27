b621e0dbef3558063bd4baa23810bc6892931aa2
Robxxt <73996637+Robxxt@users.noreply.github.com>
2024-05-27 13:48:41 +0000

    move util functions to utils.py and remove unused files

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,10 +1,1 @@
-from .yaml_version_handler import (
-    get_curr_yaml_version,
-    display_warning
-)
-from .pre_commit_handler import (
-    create_pre_commit_config_file,
-    pre_commit_requirement
-)
-
-from .utils import execute, execute_exit
+from .utils import *
--- lib/utils.py
+++ lib/utils.py
@@ -25,3 +25,47 @@ def execute(command):
         return command
     except:
         sys.exit(1)
+
+'''
+create .pre-commit-config.yaml template file
+'''
+def create_pre_commit_config_file():
+    print("""
+    [INFO]: .pre-commit-config.yaml doesn't exists.
+    Creating one based on a template. Feel free to modify it!
+    [IMPORTANT] If you modify it run: pre-commit install
+    """)
+    with open('.pre-commit-config.yaml', 'w') as file:
+        file.write('---\nThis is a template pre-commit-config file')
+
+'''
+Checks if pre_commit exists. If it doesn't informs the users and exists
+'''
+def pre_commit_requirement():
+    if (execute("pip list | grep pre-commit").returncode != 0):
+        print('[WARNING]: pre-commit is not installed')
+        try:
+            option = input('Do you want to install it [y/n]: ').upper()
+            if (option == 'Y'):
+                print('Installing pre-commit ...')
+                execute('pip install pre-commit')
+                print('Installed!')
+            else:
+                sys.exit(0)
+        except:
+            print('\nBye!')
+            sys.exit(0)
+
+'''
+Retrieves the tag with the yaml version from the repositroy and
+sets app.curr_yaml_version = "mj.fa.mn" where mj is major,
+fa is feature and mn is minimum
+'''
+def get_curr_yaml_version(app):
+    # this is temporary. Function still to be implemented
+    app.curr_yaml_version = "1.0.1"
+
+def display_warning(app):
+    print('[WARNING]: YOU SHOULD TAKE A LOOK AT pack.yaml version')
+    print(f"curr_version: {app.curr_yaml_version}")
+    print(f"pack_yaml_version: {app.pack_yaml_version}")
